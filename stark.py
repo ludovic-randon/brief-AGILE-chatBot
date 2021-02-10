fe4a7cf93458e4459c775162f2154f8284356a4e
Ludo-R <ludovic.randon@icloud.com>
2021-02-10 12:22:07 +0100

    add class

--- stark.py
+++ stark.py
@@ -1,22 +1,31 @@
 import discord
 from discord.ext import commands
+import datetime
 
 bot = commands.Bot(command_prefix = "!", description = "Bot numéro 1")
+date = datetime.datetime.now()
 
-@bot.event
-async def on_ready():
-    print("Your Bot is ready!")
+class Stark(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-@bot.event
-async def on_message(message):
-    mess = message.content
-    mess = mess.lower() 
-    if mess == "!ping":
-        await message.channel.send("Pong !")
-    if mess == "!bonjour":
-        await message.channel.send("Bonjour **%s** :smiley:" % message.author)
-    if mess == "!shutdown":
-        await message.channel.send("@everyone Bye bye !")
-        await bot.logout()   
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        mess = message.content
+        mess = mess.lower() 
+        if mess == "!ping":
+            await message.channel.send("Pong !")
+        if mess == "!date":
+            await message.channel.send("**%s**" % date)
+        if mess == "!bonjour":
+            await message.channel.send("Bonjour **%s** :smiley:" % message.author)
+        if mess == "!shutdown":
+            await message.channel.send("Bye bye !")
+            await bot.logout()
 
-bot.run("ODA4NjQ5MTU5OTI1OTU2NjI5.YCJnNw.F2YKC2ubeazi-FUClgK5H7OA9o0")  
+client = Stark()
+client.run('ODA4NjQ5MTU5OTI1OTU2NjI5.YCJnNw.F2YKC2ubeazi-FUClgK5H7OA9o0')

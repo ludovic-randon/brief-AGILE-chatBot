fb53a342806184787ab3b3e11082418584de1886
anthony <anthonybonfils3@gmail.com>
2021-02-11 10:51:31 +0100

    maj bot with nltk.Chat

--- stark.py
+++ stark.py
@@ -1,11 +1,22 @@
 import discord
 from discord.ext import commands
 import datetime
+import os
+from nltk.chat.util import Chat, reflections
+from pairs import pairs
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="config")
 
 bot = commands.Bot(command_prefix = "!", description = "Bot numéro 1")
 date = datetime.datetime.now()
 
-class Stark(discord.Client):
+class Stark(discord.Client, Chat):
+
+    def __init__(self, pairs, reflections):
+        discord.Client.__init__(self)
+        Chat.__init__(self, pairs, reflections)
+
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
@@ -13,10 +24,10 @@ class Stark(discord.Client):
         print('------')
 
     async def on_message(self, message):
-        if message.author == self.user:
-            return
         mess = message.content
         mess = mess.lower() 
+        if message.author == self.user:
+            return
         if mess == "!ping":
             await message.channel.send("Pong !")
         if mess == "!date":
@@ -26,6 +37,13 @@ class Stark(discord.Client):
         if mess == "!shutdown":
             await message.channel.send("Bye bye !")
             await bot.logout()
+        else:
+            resp = self.respond(mess)
+            if resp!=None:
+                await message.channel.send(resp)
+            else:
+                await message.channel.send("repeat please, I don't anderstand")
 
-client = Stark()
+client = Stark(pairs, reflections)
+#client.run(os.getenv("TOKEN"))  
 client.run('ODA4NjQ5MTU5OTI1OTU2NjI5.YCJnNw.F2YKC2ubeazi-FUClgK5H7OA9o0')
